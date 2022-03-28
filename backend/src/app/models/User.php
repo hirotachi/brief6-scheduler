fcff19b3e0d8d0704bf78e56b4cb5c29756d6961
Said Oudouane <saidoudouane@gmail.com>
2022-03-28 22:56:23 +0000

    create User model

---
+++ backend/src/app/models/User.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\models;
+
+class User extends Model
+{
+    protected string $table = "users";
+    protected array $required = ["lastName", "firstName", "profession", "birthdate", "authKey"];
+}
