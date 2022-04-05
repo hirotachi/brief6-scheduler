5259fe643b3c28707519b949f558c7ff901b2df0
Said Oudouane <saidoudouane@gmail.com>
2022-04-05 10:38:05 +0000

    remove frontend folder

--- frontend/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from "next/app";
-import  "@styles/styles.scss";
-
-const MyApp = (props: AppProps) => {
-  const { pageProps, Component } = props;
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
